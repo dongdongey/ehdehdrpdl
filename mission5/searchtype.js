af1382bcb28f9f01485f4b75d5680f885dd9c743
dongdongey <bananakenny@icloud.com>
2022-08-03 15:54:12 +0900

    mission5

---
+++ mission5/searchtype.js
@@ -0,0 +1,11 @@
+const old=19
+const name="dongdongey"
+const concious=true
+let gf=null
+var future=undefined
+const Iwww=2n**30n
+const sin=Symbol
+let list1=[1,2,3,4]
+
+
+console.log(typeof(list1))

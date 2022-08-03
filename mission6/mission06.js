e7159cbfd144343c578979cf082710fa28788de3
dongdongey <bananakenny@icloud.com>
2022-08-03 16:17:19 +0900

    Create mission06.js

---
+++ mission6/mission06.js
@@ -0,0 +1,14 @@
+//var 재선언, 재할당 가능
+//let 재선언 불가능, 재할당 가능
+//const 재선언, 재할당 불가능
+
+function kkbugnga(name){
+    return 'ㅋㅋ'+name+'가'
+}
+console.log(kkbugnga('버근'))
+
+let pow = (a,b,c) =>{
+    return a ** b ** c
+}
+
+console.log(pow(2,2,2))

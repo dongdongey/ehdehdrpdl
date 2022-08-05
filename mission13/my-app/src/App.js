ee809ea8850602120f6399ee13c2aa4f8687302f
dongdongey <bananakenny@icloud.com>
2022-08-05 16:05:11 +0900

    ;

--- mission13/my-app/src/App.js
+++ mission13/my-app/src/App.js
@@ -14,15 +14,6 @@ function App() {
         <div>
           {Profile()}
       </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        
-          Learn React
-        </a>
       </header>
     </div>
   );
--- mission13/my-app/src/mission14.js
+++ mission13/my-app/src/mission14.js
@@ -3,7 +3,7 @@ import React, { useState,useEffect } from 'react'
 export default function Profile () {
     const [name,setName] = useState('');
     useEffect(()=>{
-        document.title = `이름 : ${name}`  
+        document.title = `이름 : ${name}`
     },[name])
     return (
         <div>
